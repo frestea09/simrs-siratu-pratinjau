f667bc3a59b07849024e814ecc992725d0212513
Ilman teguh prasetya <ilmanfrasetya@gmail.com>
2025-08-07 02:38:34 +0000

    btw, untuk admin sismtem itu memlihiki seluruh akses di sistem ini

--- src/app/dashboard/incidents/page.tsx
+++ src/app/dashboard/incidents/page.tsx
@@ -23,7 +23,7 @@ export default function IncidentsPage() {
     setReportColumns(columns);
   };
   
-  const canViewIncidents = currentUser?.role === 'Sub. Komite Keselamatan Pasien';
+  const canViewIncidents = currentUser?.role === 'Sub. Komite Keselamatan Pasien' || currentUser?.role === 'Admin Sistem';
 
   return (
     <div className="flex-1 space-y-4 p-4 md:p-8 pt-6">

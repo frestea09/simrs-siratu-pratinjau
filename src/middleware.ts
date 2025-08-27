684031a88f3c0a1bb28032b500e2f99546a4e1a6
Ilman teguh prasetya <ilmanfrasetya@gmail.com>
2025-08-27 03:23:05 +0000

    ketika say alogin hanya mutr muter saja tidak masuk dashboard

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,16 +3,21 @@ import { NextRequest, NextResponse } from 'next/server';
 import { getSession } from '@/lib/actions/auth';
 
 const protectedRoutes = ['/dashboard'];
+const publicRoutes = ['/'];
 
 export default async function middleware(req: NextRequest) {
   const path = req.nextUrl.pathname;
   const isProtectedRoute = protectedRoutes.some((route) => path.startsWith(route));
+  
+  const session = await getSession();
 
-  if (isProtectedRoute) {
-    const session = await getSession();
-    if (!session) {
-      return NextResponse.redirect(new URL('/', req.nextUrl));
-    }
+  if (isProtectedRoute && !session) {
+    return NextResponse.redirect(new URL('/', req.nextUrl));
+  }
+  
+  // Optional: Redirect logged-in users from public routes like the login page
+  if (publicRoutes.includes(path) && session) {
+      return NextResponse.redirect(new URL('/dashboard/overview', req.nextUrl));
   }
 
   return NextResponse.next();
